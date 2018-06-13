5107b9430a72eeebe1fc118a51d17bb70b704d61
T-Bag <daotrung2409@gmail.com>
2018-06-13 11:10:42 +0700

    Add files via upload
    edit

--- Tuan02/Cau1.cpp
+++ Tuan02/Cau1.cpp
@@ -2,78 +2,59 @@
 #include<math.h>
 #include<string>
 #include <stack>
-#include<vector>
 #include <fstream>
 #include<conio.h>
 using namespace std;
 string s,t;
 int choose;
-int a[100000],n;
-void BHT(string &s, int a[]){
-	int k = 0;
-    a[0] = 0;
-    for(int i = 1 ;i < s.length(); i++){
-        while(k > 0 && s[k] != s[i]) k = a[k];
-        if(s[k] == s[i]) k = k+1;
-        a[i] = k;
-}
-}
-void search(string &s, string &t){
-	BHT(s,a);
-	int j=0,k=0;
-    int m = s.length(); int n = t.length();
-    while(j+m <= n){
-        while (k < m && s[k] == t[j+k]) k++;
-        if( k == m) {
-
-            cout << "Found in Line: " << n << "   ";
-            cout << "Colum: " << j << endl;
-        }
-        if( k == 0) j++;
-        else {
-            j = j+k-a[k-1];
-
-            k = a[k-1];     
-        }
-
-    }
-}
-int main(){
-	fstream file;
-	file.open("input.txt");
-	
-	int key =0;
-	do{
-		cout << "Chon chuc nang :" <<endl;
-    	cout << "   1.Search" <<endl;
-		cout <<  "  2.Replace"<<endl;
-		cout <<   " 3.Exit"   <<endl;
-    	cout << "-------------------------------------" << endl;
-    	cin>> key;
-    	int	n = 0;
-    	switch(key){
-    		case 1:{
-    			cout << "Enter keyword: ";
-    			string keyword;
-    			string string;
-   				cin >> keyword;
-   				while(!file.eof()){
-        			n++;
-        			getline(file,string);
-        			cout << "Line " << n << ": " << string << endl;
-       				search(keyword,string);
-    			}
-    file.close();
-				break;
-			}
-			case 2:{
-				break;
-			}
-			case 3:{
-				break;
-			}
+void Cau1(){
+	ifstream infile;
+	infile.open("BRACKET.INP", ios::in);
+	int n = 0;
+	int x;
+	stack <char> str;
+	string s;
+	infile >> s;
+	for (int i = 0; i < s.length(); i++){
+		
+		if (s[i] == '(') str.push(s[i]); //neu la ( thi dua no vao trong stack
+		
+		if(s[i] == ')'){				
+			
+			if(!str.empty())  str.pop();	// neu trong stack co ( thi xoa phan tu ( do
+			else{
+				n++;						// neu khong tang n len 1 dv 
+			}	
 		}
-	}while(key != 3);
+	}
+		x = n; // x la so ngoac dong thua
+		while(!str.empty()){
+			n++;			//dem so ngoac mo thua
+			str.pop(); 
+		}						
+		
+	//n luc nay se la so ngoac dong + mo thua
+	ofstream outfile;
+	outfile.open("BRACKET.OUT");	
+	outfile << n << endl;
+	if (n != 0)
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			if (i <= x) 						//neu i<x thi do la )
+				outfile << "(" << i - 1 << endl;		//them ( va sau vi tri )
+			else 
+				outfile << ")" << s.length() -x + i -1<< endl;
+
+		}
+	}
+	infile.close();
+	outfile.close();
+	
+}
+
+int main(){
+	Cau1();
 	return 0;
 }
 
